37b7259774dc60015917040c4d5509ab0bf18ee6
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro-2.local>
2022-09-29 11:36:20 -0400

    map

--- script.js
+++ script.js
@@ -17,6 +17,31 @@ function trackKeyClick() {
 }
 
 function ju_callback(t, email, coupon) {
+  const eventNameMap = {
+    1: 'Facebook',
+    2: 'Google Plus',
+    3: 'Twitter TWeet',
+    5: 'LinkedIn Share',
+    6: 'Twitter Follow',
+    7: 'Pinterest Follow',
+    8: 'Instagram Follow',
+    9: 'Youtube Subscribe',
+    10: 'Incentivized Form (any form integration submission)',
+    11: 'VK Follow',
+    12: 'Reddit Subscribe',
+    13: 'Google Follow',
+    14: 'LinkedIn Follow',
+    15: 'Intro Continue Click',
+    16: 'Unlocked Click',
+    17: 'Engagement - Unlocked View',
+    18: 'Engagement - Plugin',
+    19: 'Engagement - Push Notification Subscribe',
+    20: 'Engagement - Facebook Messenger Opt In',
+    21: 'Engagement - Item Added to Cart',
+    22: ' Engagement - Product Click Through',
+    23: 'Engagement - SMS Click',
+  };
+
   if (email) {
     rudderanalytics.identify(email);
     rudderanalytics.track('form submission', {
@@ -24,7 +49,7 @@ function ju_callback(t, email, coupon) {
       promotion_title: window.ju_promo_title,
       promotion_cmid: window.ju_show_campaign_id,
       // you can also add t + coupon data as well
-      t: t,
+      t: eventNameMap[t],
       coupon: coupon,
     });
   }
