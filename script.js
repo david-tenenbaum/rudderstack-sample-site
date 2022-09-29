fee0ebf941fc1781f74b3c518b3cd57ba7317638
David Tenenbaum <davidtenenbaum@Davids-MacBook-Pro-2.local>
2022-09-29 10:36:03 -0400

    test

--- script.js
+++ script.js
@@ -16,65 +16,39 @@ function trackKeyClick() {
   }
 }
 
-// function waitForElm(selector) {
-//     return new Promise(resolve => {
-//         if (document.querySelector(selector)) {
-//             return resolve(document.querySelector(selector));
-//         }
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-//         const observer = new MutationObserver(mutations => {
-//             if (document.querySelector(selector)) {
-//                 resolve(document.querySelector(selector));
-//                 observer.disconnect();
-//             }
-//         });
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
 
-//         observer.observe(document.body, {
-//             childList: true,
-//             subtree: true
-//         });
-//     });
-// }
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
 
-// waitForElm('iframe[id^="ju_iframe_"]').then((elm) => {
-//     window.addEventListener('message', (event) => {
-//         if (event.data.email) {
-//             rudderanalytics.identify(event.data.email);
-//             rudderanalytics.track('form submission', {
-//                 promotion_name: elm.name,
-//                 prompromotion_title: elm.title,
-//             });
-//         }
-//     });
-// });
-
-// function ju_callback(t,email,coupon){
-//     console.log('test: ', t,email,coupon);
-// }
-
-window.juapp =
-  window.juapp ||
-  function () {
-    (window.juapp.q = window.juapp.q || []).push(arguments);
-  };
-
-juapp(
-  'trackFunc',
-  '[impression|engagement|conversion]',
-  function (email, eventname, promotitle, cmid, options, form) {
-    console.log(email);
-    console.log(eventname);
-    console.log(promotitle);
-    console.log(cmid);
-    console.log(options);
-    console.log(form);
-
-    if (email) {
-      rudderanalytics.identify(email);
+waitForElm('iframe[id^="ju_iframe_"]').then((elm) => {
+  window.addEventListener('message', (event) => {
+    console.log('data: ', event.data);
+    if (event.data.email) {
+      rudderanalytics.identify(event.data.email);
       rudderanalytics.track('form submission', {
-        promotitle: promotitle,
-        cmid: cmid,
+        promotion_name: elm.name,
+        prompromotion_title: elm.title,
       });
     }
-  }
-);
+  });
+});
+
+function ju_callback(t, email, coupon) {
+  console.log('test: ', t, email, coupon);
+}
